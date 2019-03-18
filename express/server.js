32d6a23e6ed1124f7de6d131df415d4663b2562c
Olivier Dupain <olivier.dupain@devinci.fr>
2019-03-18 18:56:38 +0100

    update test2

--- express/server.js
+++ express/server.js
@@ -22,6 +22,11 @@ router.get('/', (req, res) => {
   res.write('<h1>C est en ligne!</h1>');
   res.end();
 });
+router.get('/henri', (req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/html' });
+  res.write('<h1>Henri en ligne!</h1>');
+  res.end();
+});
 router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
 router.post('/', (req, res) => res.json({ postBody: req.body }));
 
